039202f7a3bd5471ea6d034e83674255a811e271
ff-forth <suks17@student.bth.se>
2025-05-20 14:00:48 +0200

    bugs fixed

--- test_pickle.py
+++ test_pickle.py
@@ -1,79 +1,48 @@
 import pickle
 import random
-from tqdm import tqdm
 
 def test1(value):
-    """
-    Test the pickle and unpickle of a single value
-    """
-    for _ in range(random.randint(1, 1000)):
-        if random.randint(0, 1) == 0:
-            pic = pickle.dumps(value)
-            unpickle = pickle.loads(pic)
-        else:
-            pic = pickle.dump(value)
-            unpickle = pickle.load(pic)
-    
-    assert(value == unpickle)
+    """Test if pickling + hashing is consistent."""
+    # Hash twice and compare
+    hash1 = hash(pickle.dumps(value))
+    hash2 = hash(pickle.dumps(value))
+    assert hash1 == hash2, "Hash mismatch for same input!"
 
 def test2(value):
-    """
-    Test the pickel object of a single value
-    """
-    if random.randint(0, 1) == 0:
-        pic1 = pickle.dumps(value)
-        unpickle = pickle.loads(pic1)
-        pic2 = pickle.dumps(unpickle)
-    else:
-        pic1 = pickle.dump(value)
-        unpickle = pickle.load(pic1)
-        pic2 = pickle.dump(unpickle)
+    """Test if pickle -> unpickle -> pickle produces equivalent data."""
+    # Serialize -> deserialize -> serialize again
+    pic1 = pickle.dumps(value)
+    unpickled = pickle.loads(pic1)
+    pic2 = pickle.dumps(unpickled)
     
-    assert(pic1 == pic2)
+    # Compare the serialized bytes (should be equal for most cases)
+    assert pic1 == pic2, "Serialized data changed after roundtrip!"
 
 def generate_tuple():
-    """
-    Generate a random tuple of values
-    """
-    tuple = ()
-    for _ in range(random.randint(1, 1000)):
-        tuple += (random.randint(1, 1000),)
-    return tuple
-
+    """Generate a random tuple."""
+    return tuple(random.randint(1, 1000) for _ in range(random.randint(1, 100)))
 
 def generate_list():
-    """
-    Generate a random list of values
-    """
-    list = []
-    for _ in range(random.randint(1, 1000)):
-        list.append(random.randint(1, 1000))
-    return list
+    """Generate a random list."""
+    return [random.randint(1, 1000) for _ in range(random.randint(1, 100))]
 
 def generate_dict():
-    """
-    Generate a random dict of values
-    """
-    dict = {}
-    for _ in range(random.randint(1, 1000)):
-        dict[random.randint(1, 1000)] = random.randint(1, 1000)
-    return dict
+    """Generate a random dict."""
+    return {random.randint(1, 100): random.randint(1, 100) for _ in range(random.randint(1, 100))}
 
 def random_data_generator():
-    """
-    Generate a random tuple of values
-    """
+    """Yield random data structures indefinitely."""
+    generators = [generate_tuple, generate_list, generate_dict]
     while True:
-        yield generate_tuple()
+        yield random.choice(generators)()
 
 def main():
     random.seed(9001)
     data_generator = random_data_generator()
-    for _ in tqdm(range(1000)):
+    for _ in range(1000):
         data = next(data_generator)
         test1(data)
         test2(data)
 
 if __name__ == "__main__":
     main()
-
